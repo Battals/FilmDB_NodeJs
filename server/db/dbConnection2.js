97f89121b3456e72c3ad8180ae528a8e17806b96
Battals <batt0006@stud.kea.dk>
2023-11-14 01:09:48 +0100

    Commit

--- server/db/dbConnection2.js
+++ server/db/dbConnection2.js
@@ -1,5 +1,6 @@
 import {MongoClient} from 'mongodb'
 
+
 const uri = process.env.dbUri
 
   const client = new MongoClient(uri)
@@ -9,14 +10,15 @@ const uri = process.env.dbUri
   async function run() {
     try {
       await client.connect();
-      const db = client.db("Cluster0")
-      await db.command({ ping: 1 });
       console.log("Succesfully connected to MongoDB");
     } 
     catch(error){
 console.log(error)
     }
   }
+
+
+  
 
 
 export {client, run}
